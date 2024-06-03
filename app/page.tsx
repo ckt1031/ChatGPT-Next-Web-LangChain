1cc2bd3739842d6445ec6dc1e1320a2dd97be1be
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-03 17:30:04 +0800

    chore: Remove unused auth function and update session provider in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import { SessionProvider } from "next-auth/react";
 import { Home } from "./components/home";
-import { auth } from "./lib/auth";
 
-export default async function App() {
-  const session = await auth();
+export default function App() {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       <Home />
     </SessionProvider>
   );
