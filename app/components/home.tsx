e129bb6c4b4582a9794b7ed4087b4413c3314533
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-06-11 17:24:11 +0800

    fix build error

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -9,7 +9,7 @@ import styles from "./home.module.scss";
 import BotIcon from "../icons/bot.svg";
 import LoadingIcon from "../icons/three-dots.svg";
 
-import { getClientApi, useMobileScreen } from "../utils";
+import { useMobileScreen } from "../utils";
 
 import dynamic from "next/dynamic";
 import { ModelProvider, Path, SlotID } from "../constant";
@@ -175,6 +175,22 @@ function Screen() {
   );
 }
 
+export function getClientApi(modelName: string): ClientApi {
+  const accessStore = useAccessStore.getState();
+  if (accessStore.isUseOpenAIEndpointForAllModels) {
+    return new ClientApi(ModelProvider.GPT);
+  }
+  var api: ClientApi;
+  if (modelName.startsWith("gemini")) {
+    api = new ClientApi(ModelProvider.GeminiPro);
+  } else if (identifyDefaultClaudeModel(modelName)) {
+    api = new ClientApi(ModelProvider.Claude);
+  } else {
+    api = new ClientApi(ModelProvider.GPT);
+  }
+  return api;
+}
+
 export function useLoadData() {
   const config = useAppConfig();
   const syncStore = useSyncStore();
@@ -182,11 +198,9 @@ export function useLoadData() {
     return syncStore.cloudSync();
   }, [syncStore]);
 
-  var api: ClientApi = getClientApi(config.modelConfig.model);
-
   useEffect(() => {
     (async () => {
-      const models = await api.llm.models();
+      const models = await new ClientApi(ModelProvider.GPT).llm.models();
       config.mergeModels(models);
     })();
 
