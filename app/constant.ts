68a3717a7b2d601c501012f53854d976e36ae56d
Peter Dave Hello <hsu@peterdavehello.org>
2024-07-19 01:54:40 +0800

    Add GPT-4o mini support
    Reference:
    - https://platform.openai.com/docs/models/gpt-4o-mini
    - https://openai.com/index/gpt-4o-mini-advancing-cost-efficient-intelligence/

--- app/constant.ts
+++ app/constant.ts
@@ -134,6 +134,8 @@ export const KnowledgeCutOffDate: Record<string, string> = {
   "gpt-4-turbo-preview": "2023-12",
   "gpt-4o": "2023-10",
   "gpt-4o-2024-05-13": "2023-10",
+  "gpt-4o-mini": "2023-10",
+  "gpt-4o-mini-2024-07-18": "2023-10",
   "gpt-4-vision-preview": "2023-04",
   // After improvements,
   // it's now easier to add "KnowledgeCutOffDate" instead of stupid hardcoding it, as was done previously.
@@ -170,6 +172,8 @@ const openaiModels = [
   "gpt-4-turbo-preview",
   "gpt-4o",
   "gpt-4o-2024-05-13",
+  "gpt-4o-mini",
+  "gpt-4o-mini-2024-07-18",
   "gpt-4-vision-preview",
   "gpt-4-turbo-2024-04-09",
 ];
