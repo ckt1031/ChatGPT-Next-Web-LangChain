564e9f1d0cebad7f146103d0e5508d4004d35be9
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-07-23 22:52:41 +0800

    Use AUTH0

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,13 +1,13 @@
 import NextAuth from "next-auth";
-import ZITADEL from "next-auth/providers/zitadel";
+import Auth0 from "next-auth/providers/auth0";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: !!process.env.AUTH_ZITADEL_ID
+  providers: !!process.env.AUTH_AUTH0_ID
     ? [
-        ZITADEL({
-          issuer: process.env.AUTH_ZITADEL_ISSUER,
-          clientId: process.env.AUTH_ZITADEL_ID,
-          clientSecret: process.env.AUTH_ZITADEL_SECRET,
+      Auth0({
+          issuer: process.env.AUTH_AUTH0_ISSUER,
+          clientId: process.env.AUTH_AUTH0_ID,
+          clientSecret: process.env.AUTH_AUTH0_SECRET,
         }),
       ]
     : [],
